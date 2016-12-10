2453ff5ea903d8ce13588d86e9994d4c2f5f2782
Stephane Poirier <stephane.poirier@oifii.org>
2016-12-10 15:51:40 -0500

    first commit

---
+++ Release/spiwavwin32img.bat
@@ -0,0 +1,1 @@
+start spiwavwin32img.exe 100 100 800 800 150
---
+++ spiwavwin32img.cpp
@@ -0,0 +1,359 @@
+// spiwavwin32.cpp : Defines the entry point for the application.
+//
+
+#include "stdafx.h"
+#include "spiwavwin32img.h"
+#include "FreeImage.h"
+#include <shellapi.h> //for CommandLineToArgW()
+
+#define MAX_LOADSTRING 100
+FIBITMAP* global_dib;
+int global_x=100;
+int global_y=200;
+int global_xwidth=400;
+int global_yheight=400;
+BYTE global_alpha=200;
+
+// Global Variables:
+HINSTANCE hInst;								// current instance
+TCHAR szTitle[MAX_LOADSTRING];					// The title bar text
+TCHAR szWindowClass[MAX_LOADSTRING];			// the main window class name
+
+// Forward declarations of functions included in this code module:
+ATOM				MyRegisterClass(HINSTANCE hInstance);
+BOOL				InitInstance(HINSTANCE, int);
+LRESULT CALLBACK	WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK	About(HWND, UINT, WPARAM, LPARAM);
+
+
+PCHAR*
+    CommandLineToArgvA(
+        PCHAR CmdLine,
+        int* _argc
+        )
+    {
+        PCHAR* argv;
+        PCHAR  _argv;
+        ULONG   len;
+        ULONG   argc;
+        CHAR   a;
+        ULONG   i, j;
+
+        BOOLEAN  in_QM;
+        BOOLEAN  in_TEXT;
+        BOOLEAN  in_SPACE;
+
+        len = strlen(CmdLine);
+        i = ((len+2)/2)*sizeof(PVOID) + sizeof(PVOID);
+
+        argv = (PCHAR*)GlobalAlloc(GMEM_FIXED,
+            i + (len+2)*sizeof(CHAR));
+
+        _argv = (PCHAR)(((PUCHAR)argv)+i);
+
+        argc = 0;
+        argv[argc] = _argv;
+        in_QM = FALSE;
+        in_TEXT = FALSE;
+        in_SPACE = TRUE;
+        i = 0;
+        j = 0;
+
+        while( a = CmdLine[i] ) {
+            if(in_QM) {
+                if(a == '\"') {
+                    in_QM = FALSE;
+                } else {
+                    _argv[j] = a;
+                    j++;
+                }
+            } else {
+                switch(a) {
+                case '\"':
+                    in_QM = TRUE;
+                    in_TEXT = TRUE;
+                    if(in_SPACE) {
+                        argv[argc] = _argv+j;
+                        argc++;
+                    }
+                    in_SPACE = FALSE;
+                    break;
+                case ' ':
+                case '\t':
+                case '\n':
+                case '\r':
+                    if(in_TEXT) {
+                        _argv[j] = '\0';
+                        j++;
+                    }
+                    in_TEXT = FALSE;
+                    in_SPACE = TRUE;
+                    break;
+                default:
+                    in_TEXT = TRUE;
+                    if(in_SPACE) {
+                        argv[argc] = _argv+j;
+                        argc++;
+                    }
+                    _argv[j] = a;
+                    j++;
+                    in_SPACE = FALSE;
+                    break;
+                }
+            }
+            i++;
+        }
+        _argv[j] = '\0';
+        argv[argc] = NULL;
+
+        (*_argc) = argc;
+        return argv;
+    }
+
+int APIENTRY _tWinMain(HINSTANCE hInstance,
+                     HINSTANCE hPrevInstance,
+                     LPTSTR    lpCmdLine,
+                     int       nCmdShow)
+{
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+
+	//LPWSTR *szArgList;
+	LPSTR *szArgList;
+	int nArgs;
+	int i;
+
+	//szArgList = CommandLineToArgvW(GetCommandLineW(), &nArgs);
+	szArgList = CommandLineToArgvA(GetCommandLineA(), &nArgs);
+	if( NULL == szArgList )
+	{
+		//wprintf(L"CommandLineToArgvW failed\n");
+		return FALSE;
+	}
+	if(nArgs>1)
+	{
+		global_x = atoi(szArgList[1]);
+	}
+	if(nArgs>2)
+	{
+		global_y = atoi(szArgList[2]);
+	}
+	if(nArgs>3)
+	{
+		global_xwidth = atoi(szArgList[3]);
+	}
+	if(nArgs>4)
+	{
+		global_yheight = atoi(szArgList[4]);
+	}
+	if(nArgs>5)
+	{
+		global_alpha = atoi(szArgList[5]);
+	}
+	LocalFree(szArgList);
+ 	// TODO: Place code here.
+	MSG msg;
+	HACCEL hAccelTable;
+
+	// Initialize global strings
+	LoadString(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+	LoadString(hInstance, IDC_SPIWAVWIN32, szWindowClass, MAX_LOADSTRING);
+	MyRegisterClass(hInstance);
+
+	// Perform application initialization:
+	if (!InitInstance (hInstance, nCmdShow))
+	{
+		return FALSE;
+	}
+
+	hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_SPIWAVWIN32));
+
+	// Main message loop:
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+
+	return (int) msg.wParam;
+}
+
+
+
+//
+//  FUNCTION: MyRegisterClass()
+//
+//  PURPOSE: Registers the window class.
+//
+//  COMMENTS:
+//
+//    This function and its usage are only necessary if you want this code
+//    to be compatible with Win32 systems prior to the 'RegisterClassEx'
+//    function that was added to Windows 95. It is important to call this function
+//    so that the application will get 'well formed' small icons associated
+//    with it.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+	WNDCLASSEX wcex;
+
+	wcex.cbSize = sizeof(WNDCLASSEX);
+
+	wcex.style			= CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc	= WndProc;
+	wcex.cbClsExtra		= 0;
+	wcex.cbWndExtra		= 0;
+	wcex.hInstance		= hInstance;
+	wcex.hIcon			= LoadIcon(hInstance, MAKEINTRESOURCE(IDI_SPIWAVWIN32));
+	wcex.hCursor		= LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground	= (HBRUSH)(COLOR_WINDOW+1);
+	//wcex.lpszMenuName	= MAKEINTRESOURCE(IDC_SPIWAVWIN32); //original with menu
+	wcex.lpszMenuName	= NULL; //no menu
+	wcex.lpszClassName	= szWindowClass;
+	wcex.hIconSm		= LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+	return RegisterClassEx(&wcex);
+}
+
+//
+//   FUNCTION: InitInstance(HINSTANCE, int)
+//
+//   PURPOSE: Saves instance handle and creates main window
+//
+//   COMMENTS:
+//
+//        In this function, we save the instance handle in a global variable and
+//        create and display the main program window.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   HWND hWnd;
+
+   hInst = hInstance; // Store instance handle in our global variable
+
+   //global_dib = FreeImage_Load(FIF_PNG, "test.png", PNG_DEFAULT);
+   global_dib = FreeImage_Load(FIF_JPEG, "Brain-Cells(1200x901).jpg", JPEG_DEFAULT);
+
+   //hWnd = CreateWindow(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW, //original with WS_CAPTION etc.
+   hWnd = CreateWindow(szWindowClass, szTitle, WS_POPUP | WS_VISIBLE, //no WS_CAPTION etc.
+      //CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, NULL, NULL, hInstance, NULL);
+      //100, 200, 400, 400, NULL, NULL, hInstance, NULL);
+      global_x, global_y, global_xwidth, global_yheight, NULL, NULL, hInstance, NULL);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+	SetWindowLong(hWnd, GWL_EXSTYLE, GetWindowLong(hWnd, GWL_EXSTYLE) | WS_EX_LAYERED);
+	SetLayeredWindowAttributes(hWnd, 0, global_alpha, LWA_ALPHA);
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   return TRUE;
+}
+
+//
+//  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  PURPOSE:  Processes messages for the main window.
+//
+//  WM_COMMAND	- process the application menu
+//  WM_PAINT	- Paint the main window
+//  WM_DESTROY	- post a quit message and return
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	int wmId, wmEvent;
+	PAINTSTRUCT ps;
+	HDC hdc;
+	HGDIOBJ hOldBrush;
+	HGDIOBJ hOldPen;
+	int iOldMixMode;
+	COLORREF crOldBkColor;
+	COLORREF crOldTextColor;
+	int iOldBkMode;
+	HFONT hOldFont, hFont;
+
+	switch (message)
+	{
+	case WM_COMMAND:
+		wmId    = LOWORD(wParam);
+		wmEvent = HIWORD(wParam);
+		// Parse the menu selections:
+		switch (wmId)
+		{
+		case IDM_ABOUT:
+			DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+			break;
+		case IDM_EXIT:
+			DestroyWindow(hWnd);
+			break;
+		default:
+			return DefWindowProc(hWnd, message, wParam, lParam);
+		}
+		break;
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+		// TODO: Add any drawing code here...
+		SetStretchBltMode(hdc, COLORONCOLOR);
+		StretchDIBits(hdc, 0, 0, global_xwidth, global_yheight,
+						0, 0, FreeImage_GetWidth(global_dib), FreeImage_GetHeight(global_dib),
+						FreeImage_GetBits(global_dib), FreeImage_GetInfo(global_dib), DIB_RGB_COLORS, SRCCOPY);
+
+		hOldBrush = SelectObject(hdc, (HBRUSH)GetStockObject(GRAY_BRUSH));
+		hOldPen = SelectObject(hdc, (HPEN)GetStockObject(WHITE_PEN));
+		//iOldMixMode = SetROP2(hdc, R2_MASKPEN);
+		iOldMixMode = SetROP2(hdc, R2_MERGEPEN);
+
+		Rectangle(hdc, 100, 100, 200, 200);
+		crOldBkColor = SetBkColor(hdc, RGB(0xFF, 0x00, 0x00));
+		crOldTextColor = SetTextColor(hdc, RGB(0xFF, 0xFF, 0xFF));
+		iOldBkMode = SetBkMode(hdc, TRANSPARENT);
+		hFont=CreateFontW(70,0,0,0,FW_BOLD,0,0,0,0,0,0,2,0,L"SYSTEM_FIXED_FONT");
+		hOldFont=(HFONT)SelectObject(hdc,hFont);
+		TextOutW(hdc, 100, 100, L"test string", 11);
+
+		SelectObject(hdc, hOldBrush);
+		SelectObject(hdc, hOldPen);
+		SetROP2(hdc, iOldMixMode);
+		SetBkColor(hdc, crOldBkColor);
+		SetTextColor(hdc, crOldTextColor);
+		SetBkMode(hdc, iOldBkMode);
+		SelectObject(hdc,hOldFont);
+		DeleteObject(hFont);
+		EndPaint(hWnd, &ps);
+		break;
+	case WM_DESTROY:
+		FreeImage_Unload(global_dib);
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+}
+
+// Message handler for about box.
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	UNREFERENCED_PARAMETER(lParam);
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		return (INT_PTR)TRUE;
+
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+		{
+			EndDialog(hDlg, LOWORD(wParam));
+			return (INT_PTR)TRUE;
+		}
+		break;
+	}
+	return (INT_PTR)FALSE;
+}
